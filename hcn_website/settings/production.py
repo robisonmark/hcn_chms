c28abb60b566ec3aa9977de95fbcffdf23e60129
Matt Robison <mrobison@flogistix.com>
2023-06-09 16:53:29 -0400

    Updating the requirements and the settings files

--- hcn_website/settings/production.py
+++ hcn_website/settings/production.py
@@ -1,4 +1,5 @@
 from .base import *
+import configparser
 
 DEBUG = False
 
@@ -6,3 +7,57 @@ try:
     from .local import *
 except ImportError:
     pass
+
+parser = configparser.ConfigParser()
+parser.read("../db.conf")
+
+sections = parser.sections()
+
+SECRET_KEY = parser['django']['secrets']
+USER = parser.get("DB", "user")
+NAME = parser.get("DB", "db")
+PASSWORD = parser.get("DB", "password")
+HOST = parser.get("DB", "host")
+PORT = parser.get("DB", 'port')
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': NAME,
+        'USER': USER,
+        'PASSWORD': PASSWORD,
+        'HOST': HOST,
+        'PORT': PORT,
+    }
+}
+
+ALLOWED_HOSTS = ['.robros.dev', '138.197.100.63']
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/home/matt/hcn_chms/debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
+#CORS_ALLOWED_ORIGINS = [
+#    'https://mhacsports.com'
+#]
+
+#CSRF_TRUSTED_ORIGINS = [
+#    'https://mhacsports.com'
+#]
