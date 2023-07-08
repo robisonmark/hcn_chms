e147a6cc2133c017985a73ad7b991d06eafe9cb7
Matt Robison <baseballrocks0412@gmail.com>
2023-07-08 18:08:38 -0500

    Updating the staff model and adding in some migraitons

--- staff/models.py
+++ staff/models.py
@@ -4,6 +4,7 @@ from wagtail.api import APIField
 from wagtail.models import Page
 from wagtail.admin.panels import PageChooserPanel, FieldPanel
 from wagtail.fields import RichTextField, StreamField
+from rest_framework import serializers
 
 from streams import blocks
 
@@ -44,10 +45,19 @@ class StaffPage(Page):
         null=True,
         blank=True,
     )
+    email = models.CharField(null=False, blank=False, max_length=100, default='')
+    COMPONENT_CHOICES =  [('CircleImageStacked', 'CircleImageStacked')]
+    component = models.CharField(
+        max_length=50,
+        choices=COMPONENT_CHOICES,
+        default = 'CircleImageStacked'
+    )
+
 
     content_panels = Page.content_panels + [
         FieldPanel('given_name'),
         FieldPanel('family_name'),
+        FieldPanel('email'),
         FieldPanel('position'),
         FieldPanel('staff_bio'),
         FieldPanel('fun_facts'),
@@ -57,11 +67,38 @@ class StaffPage(Page):
 
     api_fields = [
         APIField("given_name"),
-    #     APIField("sectionHeader"),
-    #     APIField("content"),
-    #     APIField("subsections"),
+        APIField('family_name'),
+        APIField('email'),
+        APIField('position'),
+        APIField('staff_bio'),
+        APIField('fun_facts'),
+        APIField('ministry_links'),
+        APIField('image'),
+        APIField('component'),
+        APIField('email'),
+        APIField('full_name', serializer=serializers.StringRelatedField()),
+        APIField('container', serializer=serializers.StringRelatedField()),
+        APIField('tileButton', serializer=serializers.DictField()),
+        APIField('tileContent', serializer=serializers.DictField()),
     ]
 
+    @property
+    def container(self):
+        return 'Staff'
+    
+    @property
+    def tileButton(self):
+        return {
+            'url': f'mailto: {self.email}',
+            'cta': f'Contact {self.position} {self.given_name}'
+        }
+
+    @property
+    def tileContent(self):
+        return {
+            'headline': f'{self.given_name} {self.family_name}',
+            'description': self.position
+        }
 
 # class StaffPageMinistryLink(Orderable):
 #     page = ParentalKey(StaffPage, on_delete=models.CASCADE, related_name='ministry_links')
